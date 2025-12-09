e09bb4cbd76542316b25bb86c7ad01aecf6b9077
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-12-09 13:48:27 +0500

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -3,10 +3,33 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     python313
-    python313Packages.flet-desktop
-    python313Packages.flet-web
+    # python313Packages.flet-desktop
+    # python313Packages.flet-web
+    git
+    cmake
+    clang
+    ninja
+    pkg-config
     kdePackages.kdialog # filepicker
     cacert
+    which
+    gtk3
+    cairo
+    glib
+    sysprof
+    fontconfig
+    libepoxy
+    dbus
+  ];
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.gtk3
+    pkgs.glib
+    pkgs.sysprof
+    pkgs.cairo
+    pkgs.fontconfig
+    pkgs.libepoxy
+    pkgs.dbus
   ];
   shellHook = ''
     export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
